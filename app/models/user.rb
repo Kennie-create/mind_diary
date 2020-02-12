f62f2acab5a2ae58d0e97bb827c53487bbe6bd85
kennie-create <kennieboulin1989@gmail.com>
2020-02-12 13:21:09 -0500

    Defined association between journals and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :journals
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
