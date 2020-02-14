5003f646eba53566acdda49c4c9ca97cb0f6c0d2
kennie-create <kennieboulin1989@gmail.com>
2020-02-14 11:33:12 -0500

    Placed validations in order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :journals
-  
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :journals
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
