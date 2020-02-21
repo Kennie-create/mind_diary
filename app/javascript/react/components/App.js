b2ea783e453112c5a6fec1199323b13e6e314662
kennie-create <kennieboulin1989@gmail.com>
2020-02-21 10:14:14 -0500

    -Added, redifined routes

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter} from "react-router-dom"
 
+import IndexContainer from './IndexContainer'
 import JournalContainer from './JournalContainer'
 import PrescriptionContainer from './PrescriptionContainer'
 
@@ -8,6 +9,7 @@ const App = props => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/" component={IndexContainer} />
         <Route exact path="/journals" component={JournalContainer} />
         <Route exact path="/prescriptions" component={PrescriptionContainer} />
       </Switch>
