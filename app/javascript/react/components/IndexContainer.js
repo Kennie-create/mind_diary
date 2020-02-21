f1dfe3ee7f929850fcffaf434b03c351e5528e8e
kennie-create <kennieboulin1989@gmail.com>
2020-02-21 10:15:40 -0500

    -Created an IndexContainer for the welcome page

---
+++ app/javascript/react/components/IndexContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const IndexContainer = (props) => {
+
+  return(
+    <div className="welcome-page">
+      <h1 className="welcome-text">
+        Welcome to Mind Diary
+      </h1>
+
+      <div>
+        <p className="welcome-paragraph">
+          This is a zen environment, where you can express your thoughts and feelings as well as store and track your prescriptions. Be free, be happy!
+        </p>
+      </div>
+
+    </div>
+
+  )
+}
+export default IndexContainer
